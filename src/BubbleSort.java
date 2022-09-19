f898ce7713eb24e9dfb7136f2d0d4c4e70e86a38
Buggermenot <e21cseu0906@bennett.edu.in>
2022-09-19 09:05:12 +0530

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    static int[] swap(int arr[], int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        return arr;
+    }
+
+    static int[] sortBubble(int[] arr, int n){
+        boolean swapFlag = true;
+        while (swapFlag){
+//            System.out.println(Arrays.toString(arr));
+            n--;
+            swapFlag = false;
+            for (int i = 0; i < n; i++){
+                if (arr[i] > arr[i+1]){
+                    swap(arr, i, i+1);
+//                    System.out.println(Arrays.toString(arr));
+                    swapFlag = true;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,25,6,74,9,6,1};
+        int sortedArr[] = sortBubble(arr, 7);
+        System.out.println(Arrays.toString(sortedArr));
+    }
+}
